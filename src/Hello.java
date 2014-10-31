62287ae145a25dabfbae3ff433cc63c4e9263476
ryanlr <xiaoranlr@gmail.com>
2014-10-31 14:17:41 -0400

    change hello

--- src/Hello.java
+++ src/Hello.java
@@ -10,12 +10,18 @@ public class Hello {
 		list.add("java");
 		list.add("php");
 		list.add("python");
+		
+		//filter function
 		Stream<String> stream = list.stream().filter(p -> p.length() > 3);
+		
+		//? what is stream, copied collection behind? 
 		
 		//System.out.println(stream.toArray().toString());
 		for(Object a: stream.toArray()){
 			System.out.println(a.toString());
 		}
+		
+		//map function
 	}
 
 }
